9548c129c520baeca3a2d9647734caac98df5476
Billal-07 <bilhashmi@icloud.com>
2023-10-29 17:36:26 +0500

    Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from './Navbar/Navbar';
 import './App.css';
+import Sign_Up from './SignUp/SignUp';
+import Login from './Login/Login';
 
 function App() {
   return (
 <div className="App">
       
 <Navbar/>
-
-
+{/* <Sign_Up /> */}
+<Login />
 </div>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'md2': '845px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [require("rippleui")],
